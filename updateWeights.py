d2d57800e95413e51436b853e1ffe317ea28ade0
U-MSI-Kelix\Felix Li <Felix Li@MSI-Kelix.(none)>
2014-02-10 21:15:50 -0800

    added python script that helps with updating weights in config file

--- updateWeights.py
+++ updateWeights.py
@@ -2,5 +2,24 @@
 #Too time consuming to manually change
 
 f = open("updateWeights.txt", "r")
-while(line = f.readline()):
-	print line
+index = 0
+for line in f:
+	line = line.rstrip("\n")
+	if index == 0: 
+		print "weights_swipeRight = Vector(" + line + ", SWIPE_RIGHT)"
+	elif index == 1:
+		print "weights_swipeLeft = Vector(" + line + ", SWIPE_LEFT)"
+	elif index == 2:
+		print "weights_swipeUp = Vector(" + line + ", SWIPE_UP)"
+	elif index == 3:
+		print "weights_swipeDown = Vector(" + line + ", SWIPE_DOWN)"
+	elif index == 4:
+		print "weights_circle = Vector(" + line + ", CIRCLE)"
+	elif index == 5:
+		print "weights_v = Vector(" + line + ", V)"
+	elif index == 6:
+		print "weights_caret = Vector(" + line + ", CARET)"
+	elif index == 7:
+		print "weights_triangle = Vector(" + line + ", TRIANGLE)"
+
+	index += 1
